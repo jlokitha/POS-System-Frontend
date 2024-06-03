ffadae1c99d857b077481228f5b1a5a7b74a0ec5
jlokitha <lokithajanindu040130@gmail.com>
2024-06-03 22:26:00 +0530

    Code formatted

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -3,8 +3,10 @@ import { searchCustomer } from "/model/Customer.js";
 import { getAllCustomers } from "/model/Customer.js";
 
 $(document).ready(() => {
+  // Load all customers when the document is ready
   loadAllCustomers();
 
+  // JQuery selectors
   const custId = $("#cust-id");
   const custName = $("#cust-name");
   const custAddress = $("#cust-address");
@@ -15,30 +17,40 @@ $(document).ready(() => {
   const custAddressWarning = $("#cust-address-warning");
   const custSalaryWarning = $("#cust-salary-warning");
 
+  // Regular expressions
   const custIdRegex = /^C-(?!0{3})\d{3,}$/;
-  const custNameRegex = /^[A-Za-z]{3,}$/;
-  const custAddressRegex = /^(No \d+, )?[a-zA-Z]{5,}$/;
+  const custNameRegex = /^[A-Za-z]{3,}(?:\s+[A-Za-z]{3,})*$/;
+  const custAddressRegex = /(?=(?:.*[A-Za-z0-9]){5})[A-Za-z0-9'\.\-\s\,]/;
   const custSalaryRegex = /\$?\d{1,3}(,\d{3})*(\.\d{2})?(\s?K|\s?k|\s?M|\s?m)?/;
 
+  // Event listener for save button
   $("#btn-save").on("click", (event) => {
     event.preventDefault();
+    custId.val(getNewCustId());
 
-    let id = custId.val();
-    let name = custName.val();
-    let address = custAddress.val();
-    let salary = custSalary.val();
+    if (validateAll()) {
+      saveCustomer({
+        id: custId.val(),
+        name: custName.val(),
+        address: custAddress.val(),
+        salary: custSalary.val(),
+      });
 
-    saveCustomer({
-      id: id,
-      name: name,
-      address: address,
-      salary: salary,
-    });
-
-    alert("Customer Saved!!!");
-    loadAllCustomers();
+      loadAllCustomers();
+      alert("Customer Saved!!!");
+      clearInputs();
+    } else {
+      alert("Something went wrong!!!");
+    }
   });
 
+  // Event listener for clear all button
+  $("#btn-clear-all").on("click", (event) => {
+    event.preventDefault();
+    clearInputs();
+  });
+
+  // Event listeners for input validation
   custId.on("input", () => validate(custId, custIdRegex, custIdWarning));
   custName.on("input", () =>
     validate(custName, custNameRegex, custNameWarning)
@@ -50,39 +62,75 @@ $(document).ready(() => {
     validate(custSalary, custSalaryRegex, custSalaryWarning)
   );
 
+  // Function to validate all fields
+  function validateAll() {
+    let result;
+
+    result = validate(custId, custIdRegex, custIdWarning);
+
+    if (result) {
+      result = validate(custName, custNameRegex, custNameWarning);
+
+      if (result) {
+        result = validate(custAddress, custAddressRegex, custAddressWarning);
+
+        if (result) {
+          result = validate(custSalary, custSalaryRegex, custSalaryWarning);
+
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  // Validation function
   function validate(element, regex, label) {
     if (regex.test(element.val())) {
       element.css("border-color", "green");
-      label.css("display", "none");
+      label.hide();
+      return true;
     } else {
       element.css("border-color", "red");
-      label.css("display", "block");
+      label.show();
+      return false;
     }
   }
 
+  // Keydown event listener for custId
   custId.on("keydown", (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
       const id = custId.val();
 
-      if (id !== "" && id !== undefined) {
+      if (id) {
         let customer = searchCustomer(id);
 
-        custName.val(customer.name);
-        custAddress.val(customer.address);
-        custSalary.val(customer.salary);
+        if (customer) {
+          custName.val(customer.name);
+          custAddress.val(customer.address);
+          custSalary.val(customer.salary);
+        } else {
+          alert("Customer not found!!!");
+        }
+      } else {
+        custId.val("").css("border-color", "grey");
+        custIdWarning.hide();
+        custId.val(getNewCustId());
       }
+      custName.focus();
     }
   });
 
+  // Function to load all customers into the table
   function loadAllCustomers() {
     $("#cust-table tbody").empty();
 
-    for (var i = 0; i < getAllCustomers().length; i++) {
-      appendToTable(getAllCustomers()[i]);
-    }
+    getAllCustomers().forEach(appendToTable);
   }
 
+  // Function to append a customer to the table
   function appendToTable(customer) {
     $("#cust-table tbody").append(
       `<tr>
@@ -92,5 +140,31 @@ $(document).ready(() => {
         <td>${customer.salary}</td>
       </tr>`
     );
+  }
+
+  // Function to clear all input fields
+  function clearInputs() {
+    custId.val("").css("border-color", "grey");
+    custName.val("").css("border-color", "grey");
+    custAddress.val("").css("border-color", "grey");
+    custSalary.val("").css("border-color", "grey");
+
+    custIdWarning.hide();
+    custNameWarning.hide();
+    custAddressWarning.hide();
+    custSalaryWarning.hide();
+  }
+
+  // Function to generate a new customer ID
+  function getNewCustId() {
+    let customers = getAllCustomers();
+    if (customers.length === 0) {
+      return "C-001";
+    }
+
+    let currentId = customers[customers.length - 1].id;
+    let parts = currentId.split("-");
+    let num = parseInt(parts[1], 10) + 1;
+    return parts[0] + "-" + num.toString().padStart(3, "0");
   }
 });
