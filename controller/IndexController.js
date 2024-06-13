fe3dfff93af38d9e02b5c8328c7fd175ada6f06e
jlokitha <lokithajanidu040130@gmail.com>
2024-06-14 00:02:01 +0530

    Home section implemented

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -27,4 +27,57 @@ $(document).ready(() => {
     $(showSection).show();
     $("#section-name").text(secHeader);
   });
+
+  let currentSlide = 0;
+  const slideInterval = 3000; // Change slide every 3 seconds
+  let autoSlide;
+
+  function showSlide(index) {
+    const $slides = $(".carousel-item");
+    const totalSlides = $slides.length;
+
+    if (index >= totalSlides) {
+      currentSlide = 0;
+    } else if (index < 0) {
+      currentSlide = totalSlides - 1;
+    } else {
+      currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    $(".carousel-inner").css("transform", "translateX(" + offset + "%)");
+
+    $slides.each(function (i) {
+      $(this).toggleClass("active", i === currentSlide);
+    });
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  function startAutoSlide() {
+    autoSlide = setInterval(nextSlide, slideInterval);
+  }
+
+  function stopAutoSlide() {
+    clearInterval(autoSlide);
+  }
+
+  showSlide(currentSlide);
+  startAutoSlide();
+
+  $(".carousel").mouseenter(stopAutoSlide).mouseleave(startAutoSlide);
+
+  $(".prev").click(function () {
+    prevSlide();
+  });
+
+  $(".next").click(function () {
+    nextSlide();
+  });
 });
