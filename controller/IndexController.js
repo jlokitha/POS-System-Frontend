76d544d03137354a3b2cbc4e56fd72f6e33fbfed
jlokitha <lokithajanidu040130@gmail.com>
2024-06-13 12:09:17 +0530

    Change color theme | Implement navigation

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -1,15 +1,30 @@
-const sideBar = document.getElementsByClassName("side-bar")[0];
-const menu = document.getElementById("menu");
-const close = document.getElementById("close");
+$(document).ready(() => {
+  const sideBar = document.getElementsByClassName("side-bar")[0];
+  const menu = document.getElementById("menu");
+  const close = document.getElementById("close");
 
-menu.addEventListener("click", function () {
-  sideBar.classList.toggle("active");
-  menu.style.display = "none";
-  close.style.display = "block";
-});
+  menu.addEventListener("click", function () {
+    sideBar.classList.toggle("active");
+    menu.style.display = "none";
+    close.style.display = "block";
+  });
 
-close.addEventListener("click", function () {
-  sideBar.classList.toggle("active");
-  menu.style.display = "block";
-  close.style.display = "none";
+  close.addEventListener("click", function () {
+    sideBar.classList.toggle("active");
+    menu.style.display = "block";
+    close.style.display = "none";
+  });
+
+  $("section").hide();
+  $("#home").show();
+
+  $(".nav-item").click(function () {
+    $("section").hide();
+
+    let showSection = $(this).attr("href");
+    let secHeader = $(this).attr("data");
+
+    $(showSection).show();
+    $("#section-name").text(secHeader);
+  });
 });
