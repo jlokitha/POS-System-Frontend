4c54c9f980c85f9e5585489d8f6da1532b668707
jlokitha <lokithajanindu040130@gmail.com>
2024-08-08 23:46:51 +0530

    order manage connected with back end

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,11 +1,7 @@
-import { searchCustomer } from "/model/Customer.js";
-import { getAllCustomers } from "/model/Customer.js";
-import { searchItem } from "/model/Item.js";
-import { getAllItems } from "/model/Item.js";
-import { getAllOrders } from "/model/Orders.js";
-import { saveOrder } from "/model/Orders.js";
-import { updateItemQty } from "/model/Item.js";
-import { saveOrderDetail } from "/model/OrderDetails.js";
+import {searchCustomer} from "/model/Customer.js";
+import {getAllCustomers} from "/model/Customer.js";
+import {getAllItems} from "/model/Item.js";
+import {saveOrder} from "/model/Orders.js";
 
 $(document).ready(() => {
   // Elements for reuse
@@ -38,8 +34,9 @@ $(document).ready(() => {
 
   const price = /^(\$?\d{1,3}(,\d{3})*|\d+)(\.\d{2})?(\s?K|\s?k|\s?M|\s?m)?$/;
 
+  let items = [];
+
   // Load all customer ids
-  generateNextOrderId();
   setCurrentDate();
   loadAllCustomerIds();
   loadAllItemIds();
@@ -67,33 +64,42 @@ $(document).ready(() => {
 
       if ($("#added-item tbody tr").length > 0) {
         if (cashValue >= subTot) {
-          saveOrder({
-            id: orderId.val(),
-            customer: dCustomer.val(),
-            total: fullTotal.text(),
-            discount: discount.val(),
-            date: orderDate.val(),
-          });
+
+          let orderedItem = [];
 
           $("#added-item tbody tr").each(function () {
-            const iCode = $(this).find("td").eq(0).text();
-            const iQty = parseInt($(this).find("td").eq(3).text(), 10);
+            let iCode = $(this).find("td").eq(0).text();
+            let iQty = parseInt($(this).find("td").eq(3).text(), 10);
 
-            updateItemQty(iCode, iQty);
-
-            saveOrderDetail({
-              orderId: orderId.val(),
-              itemCode: iCode,
-              qty: iQty,
+            let object = findItemById(iCode);
+            object.quantity = iQty;
+            orderedItem.push({
+              itemId: parseInt(object.id),
+              description: object.description,
+              quantity: parseInt(object.quantity),
+              price: parseFloat(object.price)
             });
+            updateItemQty(iCode, iQty);
           });
+
+          const discountValue = parseFloat(discount.val()) || 0.0;
+
+          const data = {
+            date: orderDate.val(),
+            total: parseFloat(subTotal.text()),
+            discount: discountValue,
+            customerId: dCustomer.val(),
+            itemList: orderedItem
+          }
+          console.log(data, "========================")
+          saveOrder(data)
 
           alert("Order purchased!");
           clearCustomerFileds();
           clearItemFileds();
           clearPurchaseFields();
           itemTable.empty();
-          generateNextOrderId();
+          items = []
         } else {
           alert("Insuffient amount!");
         }
@@ -145,7 +151,7 @@ $(document).ready(() => {
 
     if (addNew) {
       itemTable.append(
-        `<tr>
+          `<tr>
             <td>${iCode.val()}</td>
             <td>${iDesc.val()}</td>
             <td>${iPrice.val()}</td>
@@ -220,69 +226,75 @@ $(document).ready(() => {
   function loadAllCustomerIds() {
     dCustomer.empty();
     dCustomer.append(`<option hidden></option>)`);
-    getAllCustomers().forEach((cust) => {
-      dCustomer.append(`<option value="${cust.id}">${cust.id}</option>)`);
-    });
+    getAllCustomers()
+        .then((values) => {
+          values.forEach((customer) => {
+            dCustomer.append(`<option value="${customer.id}">${customer.id}</option>)`)
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching customers:", error);
+        });
   }
 
   // Function to load all item ids
   function loadAllItemIds() {
     dItem.empty();
     dItem.append(`<option hidden></option>`);
-    getAllItems().forEach((item) => {
-      dItem.append(`<option value="${item.id}">${item.id}</option>`);
-    });
-  }
-
-  // Generate new order id
-  function generateNextOrderId() {
-    let orders = getAllOrders();
-
-    if (orders.length === 0) {
-      orderId.val("O-001");
-      return;
-    }
-
-    let currentId = orders[orders.length - 1].id;
-    let parts = currentId.split("-");
-    let no = parseInt(parts[1], 10) + 1;
-    orderId.val(parts[0] + "-" + no.toString().padStart(3, "0"));
+    getAllItems()
+        .then((values) => {
+          items = values;
+          values.forEach((item) => {
+            dItem.append(`<option value="${item.id}">${item.id}</option>`);
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching customers:", error);
+        });
   }
 
   // Fill the data of selected customer
   dCustomer.change(() => {
-    let customer = searchCustomer(dCustomer.val());
+    searchCustomer(dCustomer.val())
+        .then((value) => {
+          console.log(value)
+          cId.val(value.id);
+          cName.val(value.name);
+          cAddress.val(value.address);
+          cSalary.val(value.salary);
+        })
+        .catch((error) => {
+          console.error("Error fetching customer:", error)
+        })
 
-    cId.val(customer.id);
-    cName.val(customer.name);
-    cSalary.val(customer.salary);
-    cAddress.val(customer.address);
   });
 
   // Fill the data of selected item
   dItem.change(() => {
-    let item = searchItem(dItem.val());
-    let found = true;
+    let item = findItemById(dItem.val());
 
-    iCode.val(item.id);
-    iDesc.val(item.desc);
-    iPrice.val(item.price);
+    if (item.id === Number(dItem.val())) {
+      let found = true;
+      console.log(item)
+      iCode.val(item.id);
+      iDesc.val(item.description);
+      iPrice.val(item.price);
 
-    $("#added-item tbody tr").each(function () {
-      let iId = $(this).find("td").eq(0).text();
-      let iQty = parseInt($(this).find("td").eq(3).text(), 10);
+      $("#added-item tbody tr").each(function () {
+        let iId = $(this).find("td").eq(0).text();
+        let iQty = parseInt($(this).find("td").eq(3).text(), 10);
 
-      if (iId === item.id) {
-        found = false;
-        initialQty = item.qty - iQty;
+        if (Number(iId) === item.id) {
+          found = false;
+          initialQty = item.quantity - iQty;
+          iQtyOnH.val(initialQty);
+        }
+      });
+
+      if (found) {
+        initialQty = item.quantity;
         iQtyOnH.val(initialQty);
-        return;
       }
-    });
-
-    if (found) {
-      initialQty = item.qty;
-      iQtyOnH.val(initialQty);
     }
   });
 
@@ -323,5 +335,16 @@ $(document).ready(() => {
   // Set current date
   function setCurrentDate() {
     orderDate.val(new Date().toISOString().slice(0, 10));
+  }
+
+  // Function to find an item by ID
+  function findItemById(itemId) {
+    return items.find((item) => item.id === Number(itemId));
+  }
+
+  // Function to update item qty
+  function updateItemQty(itemId, orderedQty) {
+    let index = items.findIndex((item) => item.id === Number(itemId));
+    items[index].quantity = (items[index].quantity - orderedQty);
   }
 });
